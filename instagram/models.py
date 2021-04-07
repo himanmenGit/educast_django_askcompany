e3991f1cfda9c00f2b777e9a71204368c9f10b42
himanmenGit <study.himanmen@gmail.com>
2021-04-07 10:29:54 +0900

    애듀캐스트 리액트와 함께 장고 시작하기 AskCompany 챕터3 장고 Views (12 - URL Reverse를 통해 유연하게 URL 생성하기

--- instagram/models.py
+++ instagram/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to="instagram/post/%Y/%m/%d")
     tag_set = models.ManyToManyField("Tag", blank=True)
@@ -14,6 +17,9 @@ class Post(models.Model):
     def __str__(self):
         # return f"Custom Post Object ({self.id})"
         return self.message
+
+    def get_absolute_url(self):
+        return reverse("instagram:post_detail", kwargs={"pk": self.pk})
 
     class Meta:
         ordering = ["-id"]
