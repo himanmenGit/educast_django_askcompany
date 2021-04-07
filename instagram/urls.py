0e36372157ad0be38b11a46170fbbfb9aa5c3cfe
himanmenGit <study.himanmen@gmail.com>
2021-04-07 16:50:12 +0900

    애듀캐스트 리액트와 함께 장고 시작하기 AskCompany 챕터5 장고 Forms (05-ModelForm)

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,18 +3,19 @@ from django.urls import path, re_path, register_converter
 from . import views
 from .converters import YearConverter, MonthConverter, DayConverter
 
-
 register_converter(YearConverter, "year")
 register_converter(MonthConverter, "month")
 register_converter(MonthConverter, "day")
-
 
 app_name = "instagram"
 
 urlpatterns = [
     path("new/", views.post_new, name='post_new'),
+    path("<int:pk>/edit/", views.post_edit, name="post_edit"),
+
     path("", views.post_list, name="post_list"),
     path("<int:pk>/", views.post_detail, name="post_detail"),
+
     # path("archives/<int:year>/", views.archives_year),
     # re_path("archives/(?P<year>20\d{2})/", views.archives_year),
     # path("archives/<year:year>/", views.archives_year),
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
@@ -14,14 +15,39 @@ from .models import Post
 from .forms import PostForm
 
 
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            post = form.save()
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+
             return redirect(post)
     else:
         form = PostForm()
+
+    return render(request, 'instagram/post_form.html', {
+        'form': form,
+    })
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if post.author != request.user:
+        messages.error(request, '작성자만 수정 가능 합니다.')
+        return redirect(post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)
+    else:
+        form = PostForm(instance=post)
 
     return render(request, 'instagram/post_form.html', {
         'form': form,
